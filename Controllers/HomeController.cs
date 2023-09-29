58a47a16cf52e8eca710c4e8763ff57b8ef4f162
Rico Zethof <rico.zethof@alten.nl>
2023-09-29 12:52:47 +0200

    Windows service workin run sc create to create a service

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HardwareID;
+
+namespace WindowsServiceApiDemo.Controllers
+{
+	[ApiController]
+	[Route("Home")]
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+		private string runningMessage = "WindowsServiceApiDemo is running...";
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		[HttpGet]
+		public string Get() 
+		{
+			_logger.LogInformation(runningMessage);
+
+			try
+			{
+				SystemIdentifier systemIdentifier = new SystemIdentifier();
+
+				Console.WriteLine("CPU Identifier: " + systemIdentifier.CPUIdentifier);
+				Console.WriteLine("Disk Serial Number: " + systemIdentifier.DiskSerialNumber);
+				Console.WriteLine("BIOS Identifier: " + systemIdentifier.BIOSIdentifier);
+
+				string input = systemIdentifier.ToJson();
+				return input;
+			}
+			catch (PlatformNotSupportedException ex)
+			{
+				Console.WriteLine("Error: " + ex.Message);
+				return ex.Message;
+			}
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+using App.WindowsService;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.Extensions.Logging.EventLog;
+using System.Runtime.InteropServices;
+
+//HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddWindowsService(options =>
+{
+	options.ServiceName = ".NET Joke Service";
+});
+
+if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+{
+	LoggerProviderOptions.RegisterProviderOptions<
+	EventLogSettings, EventLogLoggerProvider>(builder.Services);
+
+	builder.Services.AddHostedService<WindowsBackgroundService>();
+}
+else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+{
+
+}
+else
+{
+	throw new PlatformNotSupportedException("Unsupported OS.");
+}
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+var host = builder.Build();
+
+host.UseHttpsRedirection();
+
+host.UseAuthorization();
+
+host.MapControllers();
+
+host.Run("http://localhost:5000");
